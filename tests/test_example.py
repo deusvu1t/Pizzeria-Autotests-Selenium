8a11ea9224e4090b296e52814d67114af5a1d207
Yury Hryshanau <mr.deusvult@gmail.com>
2026-04-25 18:27:29 +0300

    Replace example test with Google title verification test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,3 @@
-from src.pages.cart_page import CartPage
-from src.pages.main_page import MainPage
-
-
-def test_example(main_page: MainPage, cart_page: CartPage):
-    main_page.open()
-    slider = main_page.get_pizza_slider()
-    pass
+def test_google_title(driver):
+    driver.get("https://www.google.com/")
+    assert "Google" in driver.title
