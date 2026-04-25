22bdbc3c03dd2e19b2e505e97d3033e83f5a2aac
Yury Hryshanau <mr.deusvult@gmail.com>
2026-04-25 18:30:09 +0300

    Refactor Google title test to use MainPage object and assert Pizzeria title

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,6 @@
-def test_google_title(driver):
-    driver.get("https://www.google.com/")
-    assert "Google" in driver.title
+from src.pages.main_page import MainPage
+
+
+def test_google_title(main_page: MainPage):
+    main_page.open()
+    assert "Pizzeria" in main_page.get_title()
