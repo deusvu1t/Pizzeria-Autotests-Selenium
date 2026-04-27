20c058de22fc2379dd4a1bb6b65eb3dfe4b6fab6
Yury Hryshanau <mr.deusvult@gmail.com>
2026-04-28 01:38:26 +0300

    Refactor test_can_add_pizza_with_extra_option_to_cart to improve structure and readability

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -20,17 +20,23 @@ class TestProductPage:
         product_page.board_pack("Обычный")
         assert product_page.active_board_pack_value == ""
 
+    @allure.story("Добавление в корзину")
+    @allure.title("Добавление пиццы с дополнительной опцией (бортик)")
     def test_can_add_pizza_with_extra_option_to_cart(
         self, product_page: ProductPage, cart_page: CartPage
     ):
         pizza_variation = "Сырный"
         pizza_name = 'пицца "4 в 1"'
+
         product_page.open("пицца-4-в-1")
         product_page.board_pack(pizza_variation)
         pizza_price = product_page.price
+
         product_page.add_to_cart()
         product_page.header.go_to_cart()
-        item = cart_page.items()[0]
-        assert pizza_name == item.name
-        assert pizza_price == item.price
-        assert pizza_variation in item.variation
+
+        with allure.step("Проверить данные товара в корзине"):
+            item = cart_page.items()[0]
+            assert pizza_name == item.name
+            assert pizza_price == item.price
+            assert pizza_variation in item.variation
