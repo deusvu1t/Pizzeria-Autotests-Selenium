4ef39e6aee09e6ac02393064d423f26fd2972c28
Yury Hryshanau <mr.deusvult@gmail.com>
2026-04-28 02:06:10 +0300

    Implement test for cart content after adding items

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -1,5 +1,14 @@
+from src.pages.cart_page import CartPage
+from src.pages.main_page import MainPage
+
+
 class TestCartPage:
-    def test_cart_content_after_adding_items(self):
+    def test_cart_content_after_adding_items(
+        self, main_page: MainPage, cart_page: CartPage
+    ):
+        main_page.open()
+        main_page.pizza_slider().slides()[0].add_to_cart
+        main_page.pizza_slider().slides()[1].add_to_cart
         pass
 
     def test_increase_item_quantity_in_cart(self):
@@ -7,5 +16,3 @@ class TestCartPage:
 
     def test_remove_item_with_extra_option_from_cart(self):
         pass
-
-    
