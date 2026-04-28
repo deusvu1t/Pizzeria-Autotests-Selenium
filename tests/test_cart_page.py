59bd79e480c1a166ba67b0b79631eeb11c1ffffd
Yury Hryshanau <mr.deusvult@gmail.com>
2026-04-28 04:54:30 +0300

    Remove implementation details from test_cart_content_after_adding_items

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -6,9 +6,6 @@ class TestCartPage:
     def test_cart_content_after_adding_items(
         self, main_page: MainPage, cart_page: CartPage
     ):
-        main_page.open()
-        main_page.pizza_slider().slides()[0].add_to_cart
-        main_page.pizza_slider().slides()[1].add_to_cart
         pass
 
     def test_increase_item_quantity_in_cart(self):
